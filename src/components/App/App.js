983cf04f4839fcb17b9d58fa3efd096e274a241a
Nikolay Litvinenko <Litvinenko_n.v@mail.ru>
2023-06-04 12:49:17 +0300

    commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,13 @@
 import style from "./App.module.css";
-
+import FoodMenu from "../FoodMenu";
+import Nav from "../Nav/Nav";
 import Head from "../Head";
-import Nav from "../Nav";
-import FoodMenu from "../FoodMenu/FoodMenu";
-import Menu from "../Menu/Menu";
-import Basket from "../Basket";
 
 class App {
   async render() {
     Head.render();
-    Nav.render();
   }
-
 }
 
 export default new App();
+
--- src/components/FoodMenu/FoodMenu.js
+++ src/components/FoodMenu/FoodMenu.js
@@ -1,10 +1,23 @@
+
 import style from "./FoodMenu.module.css";
 
 class FoodMenu {
   constructor() {
-    this.menu = [];
-    this.basket = {};
+    this.nav = [];
+    this.menu = []
+    this.basket = {}
   }
 
-  
+  addMenuItem(menuItem) {
+    this.navMenuList.push(menuItem);
+  }
+  //Возвращает список меню 
+  getMenuList() {
+    let d = this.menuList;
+    console.log(d);
+  }
 }
+
+export default FoodMenu;
+
+
